dbd85caa79bebbf57a2f6f5cadae27bdd037ce5f
htmtmx <cesar.hpp96@hotmail.com>
2021-04-29 21:08:33 -0500

    Class GRUPOTRABAJO, USUARIO, GRUPOTRABAJOUSUARIO

---
+++ APP/model/GRUPOTRABAJO.php
@@ -0,0 +1,93 @@
+<?php
+
+
+class GRUPOTRABAJO
+{
+    private $id_gt;
+    private $id_empresa_fk;
+    private $nombre_gt;
+    private $fecha_creacion;
+    private $status;
+
+    /**
+     * @return mixed
+     */
+    public function getIdGt()
+    {
+        return $this->id_gt;
+    }
+
+    /**
+     * @param mixed $id_gt
+     */
+    public function setIdGt($id_gt): void
+    {
+        $this->id_gt = $id_gt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdEmpresaFk()
+    {
+        return $this->id_empresa_fk;
+    }
+
+    /**
+     * @param mixed $id_empresa_fk
+     */
+    public function setIdEmpresaFk($id_empresa_fk): void
+    {
+        $this->id_empresa_fk = $id_empresa_fk;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombreGt()
+    {
+        return $this->nombre_gt;
+    }
+
+    /**
+     * @param mixed $nombre_gt
+     */
+    public function setNombreGt($nombre_gt): void
+    {
+        $this->nombre_gt = $nombre_gt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFechaCreacion()
+    {
+        return $this->fecha_creacion;
+    }
+
+    /**
+     * @param mixed $fecha_creacion
+     */
+    public function setFechaCreacion($fecha_creacion): void
+    {
+        $this->fecha_creacion = $fecha_creacion;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status): void
+    {
+        $this->status = $status;
+    }
+
+
+}
---
+++ APP/model/GRUPOTRABAJOUSUARIO.php
@@ -0,0 +1,109 @@
+<?php
+
+class GRUPOTRABAJOUSUARIO
+{
+    private $id_usuario_fk;
+    private $id_gt_fk;
+    private $fecha_union;
+    private $permisos;
+    private $lider;
+    private $estado;
+
+    /**
+     * @return mixed
+     */
+    public function getIdUsuarioFk()
+    {
+        return $this->id_usuario_fk;
+    }
+
+    /**
+     * @param mixed $id_usuario_fk
+     */
+    public function setIdUsuarioFk($id_usuario_fk): void
+    {
+        $this->id_usuario_fk = $id_usuario_fk;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdGtFk()
+    {
+        return $this->id_gt_fk;
+    }
+
+    /**
+     * @param mixed $id_gt_fk
+     */
+    public function setIdGtFk($id_gt_fk): void
+    {
+        $this->id_gt_fk = $id_gt_fk;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFechaUnion()
+    {
+        return $this->fecha_union;
+    }
+
+    /**
+     * @param mixed $fecha_union
+     */
+    public function setFechaUnion($fecha_union): void
+    {
+        $this->fecha_union = $fecha_union;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPermisos()
+    {
+        return $this->permisos;
+    }
+
+    /**
+     * @param mixed $permisos
+     */
+    public function setPermisos($permisos): void
+    {
+        $this->permisos = $permisos;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLider()
+    {
+        return $this->lider;
+    }
+
+    /**
+     * @param mixed $lider
+     */
+    public function setLider($lider): void
+    {
+        $this->lider = $lider;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    /**
+     * @param mixed $estado
+     */
+    public function setEstado($estado): void
+    {
+        $this->estado = $estado;
+    }
+
+
+}
