6309dad5e4386c11a976e1b378f84a1ce13becda
Fernando <fernandohlqwe@gmail.com>
2021-05-06 19:59:08 -0500

    Agregamos constructor EMPRESA.php

---
+++ APP/model/EMPRESA.php
@@ -0,0 +1,147 @@
+<?php
+
+class EMPRESA 
+{
+    private $id_empresa;
+    private $nombre;
+    private $razon_social;
+    private $rfc;
+    private $telefono;
+    private $correo;
+    private $tipo_cuenta;
+
+    /**
+     * EMPRESA constructor.
+     * @param $id_empresa
+     * @param $nombre
+     * @param $razon_social
+     * @param $rfc
+     * @param $telefono
+     * @param $correo
+     * @param $tipo_cuenta
+     */
+    public function __construct($id_empresa, $nombre, $razon_social, $rfc, $telefono, $correo, $tipo_cuenta)
+    {
+        $this->id_empresa = $id_empresa;
+        $this->nombre = $nombre;
+        $this->razon_social = $razon_social;
+        $this->rfc = $rfc;
+        $this->telefono = $telefono;
+        $this->correo = $correo;
+        $this->tipo_cuenta = $tipo_cuenta;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getIdEmpresa()
+    {
+        return $this->id_empresa;
+    }
+
+    /**
+     * @param mixed $id_empresa
+     */
+    public function setIdEmpresa($id_empresa): void
+    {
+        $this->id_empresa = $id_empresa;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param mixed $nombre
+     */
+    public function setNombre($nombre): void
+    {
+        $this->nombre = $nombre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRazonSocial()
+    {
+        return $this->razon_social;
+    }
+
+    /**
+     * @param mixed $razon_social
+     */
+    public function setRazonSocial($razon_social): void
+    {
+        $this->razon_social = $razon_social;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRFC()
+    {
+        return $this->rfc;
+    }
+
+    /**
+     * @param mixed $rfc
+     */
+    public function setRFC($rfc): void
+    {
+        $this->rfc = $rfc;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    /**
+     * @param mixed $telefono
+     */
+    public function setTelefono($telefono): void
+    {
+        $this->telefono = $telefono;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCorreo()
+    {
+        return $this->correo;
+    }
+
+    /**
+     * @param mixed $correo
+     */
+    public function setCorreo($correo): void
+    {
+        $this->correo = $correo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTipoCuenta()
+    {
+        return $this->tipo_cuenta;
+    }
+
+    /**
+     * @param mixed $tipo_cuenta
+     */
+    public function setTipoCuenta($tipo_cuenta): void
+    {
+        $this->tipo_cuenta = $tipo_cuenta;
+    }
+
+}
