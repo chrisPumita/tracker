1334ca98dcb9c465b6cce111aa58fb43ba632e8c
Fernando <lfernandohl901@gmail.com>
2021-07-05 21:34:19 -0500

    Agregamos plantilla crear empresa

---
+++ APP/view/pl_empresa.php
@@ -0,0 +1,26 @@
+<?php
+$tittle = "Mi empresa - ProyecTracker";
+$lugar = "Crear Empresa";
+include_once "./include/header.php";
+?>
+
+    <body>
+    <div class="wrapper">
+        <?php include_once "./include/sidebar.php";?>
+        <div class="main-panel">
+            <!-- Navbar -->
+            <?php include_once "./include/navbar.php";?>
+            <div class="content">
+                <div class="container-fluid">
+                    <!-- Inicia contendo dinamico -->
+                    
+                    <!-- Termina contendo dinamico -->
+
+                </div>
+            </div>
+            <?php include_once "./include/footer.php"?>
+        </div>
+    </div>
+    </body>
+
+<?php include_once "./include/js.php"?>
