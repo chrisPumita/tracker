ceb4518026db965ff0668ae4090076dcf4cec7cb
Christan RCSG <christian.floppy@gmail.com>
2021-07-14 18:41:03 -0500

    lista categoria response

--- APP/agrega-proyecto.php
+++ APP/agrega-proyecto.php
@@ -26,7 +26,7 @@
                         <svg class="svg-inline--fa fa-table fa-w-16 me-1" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="table" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M464 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V80c0-26.51-21.49-48-48-48zM224 416H64v-96h160v96zm0-160H64v-96h160v96zm224 160H288v-96h160v96zm0-160H288v-96h160v96z"></path></svg><!-- <i class="fas fa-table me-1"></i> Font Awesome fontawesome.com -->
                         Crear proyecto.
                     </div>
-                    <div class="card-body">
+                    <div class="card-body" id="cardProyectos">
                         <form id="frm-add-new-proyect">
                             <div class="form-floating mb-3">
                                 <input class="form-control" id="nombrep" name="nombre" type="text" placeholder="Enter your first name" required="">
@@ -46,6 +46,7 @@
                             <div class="form-floating mb-3">
                                 <select class="form-select form-select-sm" id="categoria" name="categoria" type="text" required="" aria-label=".form-select-sm example">
                                 </select>
+                                <input id="imgCat" name="imgCat" type="hidden" required="">
                             </div>
                             <div class="form-floating mb-3">
                                 <input class="form-control" id="dias" name="dias" type="number" placeholder="Ingrese tiempo en dias" required="">
--- APP/ajax/list_agrega_proyecto.js
+++ APP/ajax/list_agrega_proyecto.js
@@ -2,12 +2,36 @@
 let json_cat;
 $(document).ready(function () {
     cargaListaCategorias();
-    
+    if (!cargaListGrupos()){
+        $("#cardProyectos").html("");
+        let template = `<div class="alert alert-warning " role="alert">
+                          <h4 class="alert-heading">No tienes grupos de trabajo</h4>
+                          <p>Necesitas un grupo</p>
+                          <hr>
+                          <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+                           <a href="./"><button type="button" class="btn btn-primary">Crear grupo de trabajo</button></a> 
+                        </div>`;
+
+        $('#cardProyectos').html(template);
+    }
 });
 
 
 function  cargaListGrupos(){
-
+    let boolLista = false;
+    $.ajax({
+        url: "./control/list-categorias.php",
+        success: function (response) {
+            //COnvertimos el string a JSON
+            let obj_cat = JSON.parse(response);
+            if (obj_cat.count>0){
+                boolLista = true;
+                let template = construct_list_Desp_Cat(obj_cat);
+                $('#categoria').html(template);
+            }
+        }
+    });
+    return boolLista;
 }
 
 function cargaListaCategorias(){
@@ -28,7 +52,7 @@ function cargaListaCategorias(){
 
 function construct_list_Desp_Cat(obj_cat){
     let template= '';
-    json_cat= obj_cat;
+    json_cat = obj_cat;
     obj_cat.forEach(
         objCat=>{
             template += `
@@ -36,6 +60,18 @@ function construct_list_Desp_Cat(obj_cat){
                      `;
         }
     );
-    console.log(json_cat);
     return template;
 }
+
+/// Funcion que busca en un json y coloca en un input el valor del dela propíedad objeto
+$("#categoria").change(function ()
+{
+    console.log(json_cat);
+    let idObj = $("#categoria").val();
+    json_cat.forEach(
+        cat=>{
+            if (cat.id_categoria === idObj)
+                $("#imgCat").val(cat.imagen_url);
+        }
+    );
+});
