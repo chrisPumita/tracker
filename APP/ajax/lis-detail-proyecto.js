610b95c9ff6835ee4cd1a62720c4f5e60f853bb2
Christan RCSG <christian.floppy@gmail.com>
2021-07-19 19:37:19 -0500

    vista - perfil proyecto
    Co-Authored-By: Fernando Hernandez <83323088+FernandoHL798@users.noreply.github.com>

--- APP/ajax/lis-detail-proyecto.js
+++ APP/ajax/lis-detail-proyecto.js
@@ -45,10 +45,17 @@ function constuct_grid_proyectos(obj_proyect) {
     $("#nombre_proyecto").html(obj_proyect.nombre_proyecto);
     let template =
 
-                `<div class="col-md-4">
+                `<div class="col">
                     <div class="card p-3">
-                        <div class="d-flex flex-row mb-3"><img class="icon-proyecto" src="${obj_proyect.url_imagen}" width="70">
-                            <div class="d-flex flex-column ml-2">
+                        <div class="row justify-content-center align-items-center proyecto-perfil">
+                            <div class="col-2 text-center">
+                                <div class="d-flex flex-row mb-3"><img class="icon-proyecto" src="${obj_proyect.url_imagen}" height="150">
+                                    <div class="d-flex flex-column ml-2">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-10">
                                 <span>${obj_proyect.nombre_proyecto}</span>
                                 <span class="text-black-50">${obj_proyect.nombre_gt}</span>
                                 <span class="ratings">
@@ -58,12 +65,14 @@ function constuct_grid_proyectos(obj_proyect) {
                                         </div>
                                     </div>
                                 </span>
+                                <h5>Fecha de creacion: ${obj_proyect.fecha_creacion}</h5>
+                                <div class="d-flex justify-content-between install mt-3">
+                                    <span>Tiempo: ${obj_proyect.dias} dias</span>
+                                </div>
                             </div>
                         </div>
-                        <h5>Fecha de creacion: ${obj_proyect.fecha_creacion}</h5>
-                        <div class="d-flex justify-content-between install mt-3">
-                            <span>Tiempo: ${obj_proyect.dias} dias</span>
-                        </div>
+
+                        
                     </div>
                 </div>`;
                 $("#grd-proyecto").html(template);
@@ -78,17 +87,17 @@ function constructEtapas(obj_proyect){
             contador++;
             template += `
             <!-- start  card lista etapa --->
-                <div class="card mb-4">
+                <div class="card">
                     <div class="card-header">
                         <svg class="svg-inline--fa fa-table fa-w-16 me-1" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="table" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M464 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V80c0-26.51-21.49-48-48-48zM224 416H64v-96h160v96zm0-160H64v-96h160v96zm224 160H288v-96h160v96zm0-160H288v-96h160v96z"></path></svg><!-- <i class="fas fa-table me-1"></i> Font Awesome fontawesome.com -->
                         Etapa ${contador}  : ${objProyect.nombre_etapa}
-                        <div class="row d-flex justify-content-center align-items-center">
-                            <div class="col-10">
+                        <div class="row d-block justify-content-center align-items-center">
+                            <div class="col">
                                 <div class="progress">
                                     <div class="progress-bar  progress-bar-striped bg-info" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
                                 </div>
                             </div>
-                            <div class="col-2">
+                            <div class="col py-3">
                                 <!-- Button trigger modal -->
                                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalAddSubetapa">
                                     Agrega Sub-etapa
