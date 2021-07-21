5b10dab215ea697158056e01db911eca8c2e9d23
Fernando <lfernandohl901@gmail.com>
2021-07-21 12:00:49 -0500

    Apartado grupo de trabajos con conexion a DB

--- APP/modals/modal-edita-proyecto.php
+++ APP/modals/modal-edita-proyecto.php
@@ -197,3 +197,28 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="modalCreateGT" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Nuevo grupo de trabajo</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+
+            <div class="modal-body">
+            <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="floatingInput" placeholder="Nombre del Proyecto">
+                    <label for="floatingInput">Nombre del proyecto</label>
+                </div>
+            </div>
+
+
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            <button type="button" class="btn btn-primary">Crear</button>
+            </div>
+        </div>
+    </div>
+</div>
