98ffba47cb2eda9745751fdc7756a9807d59f173
Fernando <lfernandohl901@gmail.com>
2021-07-26 19:52:38 -0500

    Boton cambair contraseña perfil usuario

--- APP/control/usuario-detalles.php
+++ APP/control/usuario-detalles.php
@@ -1,5 +1,8 @@
 <?php
-include_once "control_usuario.php";
-$idUser= 5;
-$result = detalleUser($idUser);
-var_dump($result);
+if(isset($_POST['idUser'])){
+
+    $idUser= $_POST['idUser'];
+    include_once "./control_usuario.php";
+    $result = detalleUser($idUser);
+    echo $result;
+} else echo "datos incompletos";
